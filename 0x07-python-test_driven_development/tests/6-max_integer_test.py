0fcd1826e2bbab921e97421e35a6832411ebd487
Anki Gilbert Okosso <ankigilberokosso@gmail.com>
2023-07-11 06:56:04 -0700

    update

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -46,7 +46,7 @@ class TestMaxInteger(unittest.TestCase):
     def test_string(self):
         """This is a Test a string."""
         string = "Gilbert"
-        self.assertEqual(max_integer(string), 'r')
+        self.assertEqual(max_integer(string), 'i')
 
     def test_list_of_strings(self):
         """This is a Test a list of stringsd."""
