53611ae0dcb3c912ad0d7b3ed9979e7684b42200
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2023-09-29 18:58:49 -0700

    python script that fetches

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,8 +8,8 @@
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        cont = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as re:
+        cont = re.read()
         print("Body response:")
         print("\t- type: {}".format(type(cont)))
         print("\t- content: {}".format(cont))
