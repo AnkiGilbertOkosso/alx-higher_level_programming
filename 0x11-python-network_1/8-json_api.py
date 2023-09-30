5b0ee2770b176969d5712cbc0f5c21b09a5bfd27
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2023-09-29 19:49:58 -0700

    task8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A script tha:
+- takes in a letter
+- sends POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    let = "" if len(sys.argv) == 1 else sys.argv[1]
+    pay = {"q": let}
+
+    re = requests.post("http://0.0.0.0:5000/search_user", data=pay)
+    try:
+        res = re.json()
+        if res == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(res.get("id"), res.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
