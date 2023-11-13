0757dd326cb6671f8bd53ece2afc0fa3933165ae
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2023-11-13 08:45:11 -0800

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  const first_arr = process.argv.slice(2).map(Number);
-  const second_arr = first_arr.sort(function (a, b) { return b - a; })[1];
-  console.log(second_arr);
+  const firstArr = process.argv.slice(2).map(Number);
+  const secondArr = firstArr.sort(function (a, b) { return b - a; })[1];
+  console.log(secondArr);
 }
