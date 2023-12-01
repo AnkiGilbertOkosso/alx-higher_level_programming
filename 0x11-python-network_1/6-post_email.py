8859e144c7554655821cc83811486f40ef278123
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2023-12-01 12:10:22 -0800

    task 7

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,11 +5,19 @@ sends a POST request to the passed URL with the email
 as a parameter, and finally displays the body of the response
 """
 import sys
-import urllib.request
+import requests
+
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <URL> <email>")
+        sys.exit(1)
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+
+    print("Response Body:")
+    print(response.text)
