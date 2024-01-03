d56ba569f04655326406f270dffc042257c6f491
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-01-03 11:01:03 -0800

    updates

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,21 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
+const httpRequest = require('request');
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+httpRequest(apiUrl, function (error, response, body) {
   if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+    const charactersList = JSON.parse(body).characters;
+    printCharacters(charactersList, 0);
   }
 });
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
+function printCharacters(charactersList, index) {
+  httpRequest(charactersList[index], function (error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
+      if (index + 1 < charactersList.length) {
+        printCharacters(charactersList, index + 1);
       }
     }
   });
-}i
+}
