81bb1eb8b73190411d285e5b885ac5946c322877
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-01-03 10:21:10 -0800

    task 100

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const httpRequest = require('request');
+const filmId = process.argv[2];
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
+
+httpRequest.get(apiUrl + filmId, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+  const filmData = JSON.parse(body);
+  const charactersUrls = filmData.characters;
+  for (const characterUrl of charactersUrls) {
+    httpRequest.get(characterUrl, function (err, res, body1) {
+      if (err) {
+        console.log(err);
+      }
+      const characterData = JSON.parse(body1);
+      console.log(characterData.name);
+    });
+  }
+});
