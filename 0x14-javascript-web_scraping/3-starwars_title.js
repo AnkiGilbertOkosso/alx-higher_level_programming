50fd3cd7968d855ae768e8c9ff2b6588f49b7759
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-01-03 10:13:16 -0800

    task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const httpRequest = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+
+httpRequest(API_URL + episodeNum, function (error, res, body) {
+  if (error) {
+    console.log(error);
+  } else if (res.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + res.statusCode);
+  }
+});
